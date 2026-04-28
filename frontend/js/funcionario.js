f0a19af4f4f3f933f8797caf280b22fe25a80e17
Larissa Furlan <164415172+Larifurlan@users.noreply.github.com>
2026-04-28 08:15:35 -0300

    feat: adding register employee modal

--- frontend/js/funcionario.js
+++ frontend/js/funcionario.js
@@ -13,6 +13,68 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         renderEmployees(funcionariosFiltrados);
     });
+
+    const modal = document.getElementById("employeeModal");
+
+    document.getElementById("btnOpen").addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+
+    document.getElementById("btnCancelModal").addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
+    document.getElementById("employeeForm").addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const name = document.getElementById("empName").value.trim();
+        const email = document.getElementById("empEmail").value.trim();
+        const password = document.getElementById("empPassword").value.trim();
+
+        if (!name || !email || !password) {
+            showAlert('warning', 'CAMPOS OBRIGATÓRIOS', 'Preencha todos os campos!');
+            return;
+        }
+
+        try {
+            const token = localStorage.getItem("token");
+
+            const response = await fetch("http://localhost:8080/user", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + token,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                    role: "EMPLOYEE" 
+                })
+            });
+
+            if (response.ok) {
+                showAlert('success', 'SUCESSO', 'Funcionário cadastrado!');
+
+                modal.style.display = "none";
+                e.target.reset();
+
+                await loadEmployees(); 
+            } else {
+                const err = await response.json();
+                showAlert('error', 'ERRO', err.message);
+            }
+
+        } catch (error) {
+            showAlert('error', 'ERRO', 'Falha na conexão com servidor');
+        }
+    });
 });
 
 let allEmployees = [];
@@ -36,7 +98,7 @@ async function loadEmployees() {
             renderEmployees(allEmployees);
         }
     } catch (error) {
-        showAlert('error', 'ERRO', 'Não foi possível carregar o funcionarios.');
+        showAlert('error', 'ERRO', 'Não foi possível carregar os funcionários.');
     }
 }
 
@@ -68,12 +130,12 @@ function renderEmployees(employees) {
 
                 <div class="actions">
                     <button class="edit">
-                        <img src="../assets/icon_lapis.png" alt="Editar">
+                        <img src="../assets/icon_lapis.png">
                         <span>Editar</span>
                     </button>
 
                     <button class="delete">
-                        <img src="../assets/icon_lixeira.png" alt="Excluir">
+                        <img src="../assets/icon_lixeira.png">
                         <span>Excluir</span>
                     </button>
                 </div>
